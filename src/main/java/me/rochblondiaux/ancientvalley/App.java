95a6b311c21d71d4aa1db9a68161d6e258e519a9
Roch <roch.blondiaux@protonmail.com>
2022-10-04 23:18:52 +0200

    Project structure

---
+++ src/main/java/me/rochblondiaux/ancientvalley/App.java
@@ -0,0 +1,13 @@
+package me.rochblondiaux.ancientvalley;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
